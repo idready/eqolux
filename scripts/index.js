cd50ddbf904ffe0c3e9cb2b742e047e7a60b17d2
idready <you_sombo@hotmail.fr>
2025-11-02 14:06:24 +0100

    Hide scrolling on drawer display

--- scripts/index.js
+++ scripts/index.js
@@ -3,17 +3,16 @@ const main = () => {
     const openMenuBtn = document.getElementById('open-menu');
     const drawerBlock = document.querySelector('.drawer-block');
     const closeMenuBtn = document.getElementById('close-menu');
+    const body = document.body;
 
     openMenuBtn.addEventListener('click', () => {
         drawerBlock.classList.toggle('active');
+        body.classList.toggle('overflow-hidden');
     });
-
-    // drawerBlock.addEventListener('click', () => {
-    //     drawerBlock.classList.remove('active');
-    // });
 
     closeMenuBtn.addEventListener('click', () => {
         drawerBlock.classList.remove('active');
+        body.classList.remove('overflow-hidden');
     });
 }
 
